4bc24de426ced9877cab251ca9a6e4d7cce937fe
sindeedindeed <sindeed222@gmail.com>
2025-05-24 09:09:53 +0600

    Basic setup + hello world!

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-gray-600 text-white p-4'>Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
